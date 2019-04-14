f79913e5476551ffae87bafae0d5e7d0f0f59761
Mark <koekto93@mail.ru>
2019-04-14 15:59:35 +0300

    первые потуги для jwt

--- trip-server/libs/passport/strategies/authenticate.js
+++ trip-server/libs/passport/strategies/authenticate.js
@@ -1,18 +1,21 @@
 const User = require('../../../models/User');
 
 module.exports = async function authenticate(strategy, email, profile, done) {
-  User.findOne({email}, (err, user) => {
+  User.findOne({ email }, (err, user) => {
     if (err) return done(err);
-    
+
     if (!user) {
-      User.create({
-        email,
-        displayName: profile.displayName || profile.username,
-        providers: [{id: strategy, profile}]
-      }, (err, user) => {
-        if (err) return done(err);
-        done(null, user, { message: 'Добро пожаловать!' });
-      });
+      User.create(
+        {
+          email,
+          displayName: profile.displayName || profile.username,
+          providers: [{ id: strategy, profile }],
+        },
+        (err, user) => {
+          if (err) return done(err);
+          done(null, user, { message: 'Добро пожаловать!' });
+        },
+      );
     } else {
       if (user.providers.find(provider => provider.id === strategy)) {
         done(null, user, { message: 'Добро пожаловать!' });
@@ -20,7 +23,7 @@ module.exports = async function authenticate(strategy, email, profile, done) {
         user.providers.push({ id: strategy, profile });
         user.save(err => {
           if (err) return done(err);
-          
+
           done(null, user, { message: 'Добро пожаловать!' });
         });
       }
